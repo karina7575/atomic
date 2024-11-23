be122d7ac00ff6f46d7870bb6c8c0269697527fa
Karina <[karinakios@mail.ru]>
2024-11-23 15:22:25 +0300

    Initial commit

---
+++ src/test/java/com/javaacademy/atomic/AtomicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaacademy.atomic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
